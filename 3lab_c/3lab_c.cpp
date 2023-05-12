88b1ea975ee3484cc909bba4c9aa32efaf9e1be6
daniya_kuspanova <kyspanova@icloud.com>
2023-05-12 18:07:42 +0400

    add removing funtions : accurate!!!!

--- 3lab_c/3lab_c.cpp
+++ 3lab_c/3lab_c.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <map>
 #include <vector>
+#include <iterator>
 using namespace std;
 template<typename vertex_type, typename Distance = double>
 class Graph {
@@ -13,20 +14,22 @@ private:
         Edge(vertex_type id1_ = 0,vertex_type id_2 = 0, Distance distance_ = 0) : id1(id1_), id2(id_2), dist(distance_){}
     };
     int n;
-    std::map<vertex_type, vector<Edge>> map_v;
+    std::map<vertex_type, std::map<int,Edge>> map_v;
 
 public:
-    Graph(int n_vert = 0) : n(n_vert){}
-    
+    Graph(int n_vert = 0) : n(n_vert) {}
+
     friend std::ostream& operator<< (std::ostream& out, const Edge& e)
     {
         out << "-(" << e.id1 << ", " << e.id2 << "; " << e.dist << ')';
         return out;
     }
-    friend std::ostream& operator<< (std::ostream& out, const std::vector<Edge>&vec)
+    friend std::ostream& operator<< (std::ostream& out, const std::map<int, Edge>& map)
     {
-        for(int i = 0; i < vec.size();i++)
-        out << "[" << i << "] " << vec[i];
+        for (auto it = map.begin(); it != map.end(); ++it)
+        {
+            out << it->first << ':' << it->second << endl;
+        }
         return out;
     }
     //проверка-добавление-удаление вершин
@@ -40,7 +43,7 @@ public:
     {
         if (!has_vertex(v))
         {
-            std::vector<Edge> mass;
+            std::map<int, Edge> mass;
             map_v[v] = mass;
             n++;
             return true;
@@ -52,6 +55,17 @@ public:
         if (has_vertex(v))
         {
             map_v.erase(v);
+            for (auto it1 = map_v.begin(); it1 != map_v.end(); ++it1)
+            {
+                for (auto it2 = it1->second.begin(); it2 != it1->second.end(); ++it2)
+                {
+                    if (it2->second.id2 == v)
+                    {
+                        map_v[it1->first].erase(it2);
+                        break;
+                    }
+                }
+            }
             return true;
         }
         return false;
@@ -63,12 +77,10 @@ public:
     {
         if (has_vertex(from) && has_vertex(to))
         {
-            std::vector<Edge>tmp = map_v[from];
-            for(int i = 0; i < tmp.size(); i++)
+            for (auto it = map_v[from].begin(); it != map_v[from].end(); ++it)
             {
-                if (tmp[i].id2 == to) return true;
+                if (it->second.id2 == to) return true;
             }
-            return false;
         }
         return false;
     }
@@ -76,12 +88,10 @@ public:
     {
         if (has_vertex(e.id1) && has_vertex(e.id2))
         {
-            std::vector<Edge>tmp = map_v[e.id1];
-            for (int i = 0; i < tmp.size(); i++)
+            for (auto it = map_v[e.id1].begin(); it != map_v[e.id1].end(); ++it)
             {
-                if (tmp[i].id2 == e.id2 && tmp[i].dist == e.dist) return true;
+                if (it->second.id2 == e.id2 && it->second.dist == e.dist) return true;
             }
-            return false;
         }
         return false;
     }
@@ -92,7 +102,9 @@ public:
         {
             if (has_vertex(from) && has_vertex(to))
             {
-                map_v[from].push_back(tmp);
+                auto it = map_v[from].begin();
+                int n_map = 0;
+                map_v[from][map_v[from].size()] = tmp;
                 return true;
             }
         }
@@ -105,10 +117,39 @@ public:
             cout << it->first << ':' << it->second << endl;
         }
     }
-    /*bool remove_edge(const vertex_type& from, const vertex_type& to)
-    {
+    //bool remove_edge(const vertex_type& from, const vertex_type& to) { // удаление ребра
+    //    std::map<int, Edge>::std::iterator it;
+    //    for (it = map_v[from].begin(); it != map_v[from].end(); it++) {
+    //        if (it->second.id2 == to) {
+    //            map_v[from].erase(it);
+    //            return true;
+    //        }
+    //    }
+    //    return false;
+    //}
+    //bool remove_edge(const vertex_type& from, const vertex_type& to) { // удаление ребра
+    //    map<vertex_type, std::vector<Edge>>::iterator it, itEnd;
+    //    it = map_v.begin();
+    //    itEnd = map_v.end();
+    //    while (it != itEnd)
+    //    {
+    //        
+    //        it++;
+    //    }
 
-    }*/
+    //    for (it = adj[v].begin(); it != adj[v].end(); it++) {
+    //        if (*it == w) {
+    //            adj[v].erase(it);
+    //            break;
+    //        }
+    //    }
+    //    for (it = adj[w].begin(); it != adj[w].end(); it++) {
+    //        if (*it == v) {
+    //            adj[w].erase(it);
+    //            break;
+    //        }
+    //    }
+    //}
     //bool remove_edge(const Edge& e); //c учетом расстояния
     //
 
@@ -128,22 +169,22 @@ public:
 
 int main()
 {
-    /*std::cout << "Hello World!\n";
-    std::vector<int> m;
-    m.resize(0);*/
     Graph<int> tmp;
     tmp.add_vertex(1);
     tmp.add_vertex(2);
     tmp.add_vertex(3);
     tmp.add_vertex(4);
-    tmp.remove_vertex(5);
-    tmp.remove_vertex(3);
     tmp.add_edge(1, 2, 16);
     tmp.add_edge(2, 6, 2);
     tmp.add_edge(1, 3, 2);
     tmp.add_edge(3, 2, 9);
     tmp.add_edge(3, 4, 5);
     tmp.add_edge(4, 1, 8);
+    tmp.Print();
+    tmp.remove_vertex(5);
+    tmp.remove_vertex(3);
+    
+   /* tmp.remove_edge(1, 2);*/
     tmp.Print();
     return 0;
 }
